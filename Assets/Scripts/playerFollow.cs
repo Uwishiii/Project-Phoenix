55e01f9fe65e1cabe3c9466c46793979b02c219e
Kirirgiri <95420741+Kirirgiri@users.noreply.github.com>
2022-11-09 11:47:02 +0100

    continuous movement + spidey

--- Assets/Scripts/playerFollow.cs
+++ Assets/Scripts/playerFollow.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class playerFollow : MonoBehaviour
 {
+    public Transform player;
+    public float speed = 4f;
+
+
+    private Rigidbody rig;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 towards = new Vector3(player.rotation.x, player.rotation.y, player.rotation.z-90.0f);
+        Vector3 pos = Vector3.MoveTowards(transform.position, player.position, speed * Time.fixedDeltaTime);
+        rig.MovePosition(pos);
+        transform.LookAt(towards);
     }
 }
